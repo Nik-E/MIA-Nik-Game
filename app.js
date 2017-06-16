e95685f621aa9a979cbed41b8bcfc8702f44450a
Nik E <NikolasEchenique@gmail.com>
2017-06-16 16:17:45 -0400

    submission for presentation

--- app.js
+++ app.js
@@ -1,29 +1,90 @@
 console.log('APP.JS ONLINE');
 
-// $(document).ready(function(){
-//   $('#game').hide();
-//   $('#start-button').one('click',function (){
-//       $('#start-screen').fadeOut();
-//       $('#game').fadeIn();
-//
-//       startGame();
-//   });
-// });
+ion.sound({
+    sounds: [
+        {
+            name: "Borrtex_-_05_-_Attainment",
+        },
+        {
+            name: "glass",
+            volume: 0.5,
+        }
 
+    ],
+    volume:0.8,
+    path: 'ion.sound-3.0.7-copy/sounds/',
+    multiplay: true,
+    preload: true
+});
+
+$(document).ready(function(){
+  ion.sound.play("Borrtex_-_05_-_Attainment");
+  $('#game').hide();
+  $('#game-over-screen').hide();
+  $('#start-button').one('click',function (){
+      $('#start-screen').fadeOut();
+      $('#game').fadeIn();
+      playGame();
+      function startTimer(){
+        var hou = 0;
+        var sec = 30;
+        var timer = setInterval(function(){
+          var displaySec = sec;
+
+          if (0 <= sec && sec < 10) {
+            displaySec = "0" + sec;
+          }
+
+          document.getElementById("timer").innerHTML = "0"+hou +":" + displaySec;
+          sec--;
+
+          if(sec < 0)
+          {
+            sec = 0;
+            clearInterval(timer);
+            $('#game').fadeOut();
+            $('#game-over-screen').fadeIn();
+            $('#play-again').one('click',function (){
+                document.location.reload();
+            });
+          }
+        }, 1000);
+      }
+      startTimer();
+  });
+});
+function playGame(){
+
+
+  //// TEST UR SHIT BOIIIII
+
+  // document.addEventListener('click', function(event) {
+  //     ion.sound.play("glass");
+  //     var theX = event.pageX,
+  //     theWhy = event.pageY,
+  //     theOffsetX = event.pageX - canvasLeft;
+  //     theOffsetY = event.pageY - canvasTop;
+  //     console.log('THE X' + theX);
+  //     console.log('THE WHY' + theWhy);
+  //     console.log('THE OFFSET X' + theOffsetX);
+  //     console.log('THE OFFSET WHY' + theOffsetY);
+  //
+  // });
+  ////
 //----------------DO NOT TOUCH, THIS SHIT WORKS--------------
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 var scroll = -65;
 var scrollDX = 2;
-
+var offsetParent = 0;
 var canvasLeft = canvas.offsetLeft;
 var canvasTop = canvas.offsetTop;
 var elements = [];
 
 var bonus = 1;
 var score = 0;
-var speed = 100;
+// var speed = 100;
 //-----------------------------------------------------------
 
 //---------------Beautiful, Amazing, Gold. I swear to God if you fucking touch this code.........-------------------
@@ -41,16 +102,15 @@ function MovingShapes (){
 }
 
 MovingShapes.prototype.drawSelectedShape =
-
   function (){
-var shapePicked = this.shapeType;
-var myTop = this.top;
-var selectedShapeImg = this.selectedShapeImg;
+    var shapePicked = this.shapeType;
+    var myTop = this.top;
+    var selectedShapeImg = this.selectedShapeImg;
 
 
     function draw(ctx,selectedShapeImg){
 
-      // console.log("non stop shit: ", shapePicked.type, myTop);
+
 
       if (!selectedShapeImg.complete){
         setTimeout(function(){
@@ -64,7 +124,7 @@ var selectedShapeImg = this.selectedShapeImg;
 
     draw(ctx,selectedShapeImg);
 
-};
+  };
 var shapes = [
     {
      type: "Triangle-PNG-Pic.png",
@@ -74,7 +134,7 @@ var shapes = [
      left: 5,
      scoreEffect: +2,
      bonusEffect: +0.5,
-     speedEffect: +1,
+    //  speedEffect: +1,
 
    },
    {
@@ -83,9 +143,9 @@ var shapes = [
     height: 65,
     top: 0,
     left: 4,
-    scoreEffect: -1,
-    bonusEffect: -0.25,
-    speedEffect: -0.25,
+    scoreEffect: +1,
+    bonusEffect: +0.25,
+    // speedEffect: -0.25,
 
   },
   {
@@ -96,7 +156,7 @@ var shapes = [
    left: 3.5,
    scoreEffect: +2,
    bonusEffect: +0.5,
-   speedEffect: +0.5,
+  //  speedEffect: +0.5,
 
   },
   {
@@ -106,8 +166,8 @@ var shapes = [
    top: 0,
    left: 3.5,
    scoreEffect: +3,
-   bonusEffect: -2,
-   speedEffect: -5,
+   bonusEffect: +2,
+  //  speedEffect: -5,
 
   },
   {
@@ -116,9 +176,9 @@ var shapes = [
    height: 65,
    top: 0,
    left: 3.5,
-   scoreEffect: -10,
+   scoreEffect: +10,
    bonusEffect: +5,
-   speedEffect: +5,
+  //  speedEffect: +5,
 
   }
   ];
@@ -128,7 +188,7 @@ var shapes = [
 
 
 
-function makeFirstFive() {
+// function makeFirstFive() {
     setTimeout(function(){
       var shape1 = new MovingShapes();
       shape1.drawSelectedShape();
@@ -155,9 +215,9 @@ function makeFirstFive() {
       elements.push(shape5);
     },7000);
 
-  }
+  // }
 
-makeFirstFive();
+// makeFirstFive();
 setInterval(function(){
 
 
@@ -190,7 +250,7 @@ setInterval(function(){
 
 },7500);
 
-var elements = [];
+
 
 var interval = setInterval(function() {
   ctx.clearRect(0, 0, innerWidth, innerHeight);
@@ -198,7 +258,7 @@ var interval = setInterval(function() {
   var updatedElements = [];
 
   elements.forEach(function (element) {
-    element.top += scrollDX;
+
     element.drawSelectedShape();
 
 
@@ -207,6 +267,7 @@ var interval = setInterval(function() {
 
 
     if (element.top < innerHeight) {
+        element.top += scrollDX;
       updatedElements.push(element);
     }
   });
@@ -221,8 +282,12 @@ var interval = setInterval(function() {
 //-----------Click functionality------
  // Add event listener for `click` events.
  canvas.addEventListener('click', function(event) {
+     ion.sound.play("glass");
      var x = event.pageX - canvasLeft,
          y = event.pageY - canvasTop;
+
+
+
 
 
  // Collision detection between clicked offset and element
@@ -233,7 +298,8 @@ var interval = setInterval(function() {
        var shapeWidth = element.width;
        var shapeScoreEffect = element.scoreEffect;
        var shapeBonusEffect = element.bonusEffect;
-       var shapeSpeedEffect = element.speedEffect;
+      //  var shapeSpeedEffect = element.speedEffect;
+
 
          if (y > shapeTop && y < shapeTop + shapeHeight && x > shapeLeft && x < shapeLeft + shapeWidth) {
 
@@ -241,41 +307,17 @@ var interval = setInterval(function() {
               var scmult = document.getElementById('scmult');
               scmult.innerHTML=bonus;
 
-              score=((score+shapeScoreEffect) * bonus);
+              score=(score+(shapeScoreEffect * bonus));
               var points = document.getElementById('points');
               points.innerHTML=score;
+              var gameOverPoints = document.getElementById('total-points');
+              gameOverPoints.innerHTML=score;
 
-              speed=speed+shapeSpeedEffect;
-              var spmult =document.getElementById('spmult');
-              spmult.innerHTML=speed;
+              // speed=speed+shapeSpeedEffect;
+              // var spmult =document.getElementById('spmult');
+              // spmult.innerHTML=speed;
          }
      });
    }, false);
-//--------------End of Click functionality---
-//---------------------------------------------------------------------------------
 
-
-
-
-//-------Timer---------------
-window.onload = function(){
-  var hou = 0;
-  var sec = 30;
-  var timer = setInterval(function(){
-    var displaySec = sec;
-
-    if (0 <= sec && sec < 10) {
-      displaySec = "0" + sec;
-    }
-
-    document.getElementById("timer").innerHTML = "0"+hou +":" + displaySec;
-    sec--;
-
-    if(sec < 0)
-    {
-      sec = 0;
-      clearInterval(timer);
-    }
-  }, 1000);
-};
-//---------------------------
+}
